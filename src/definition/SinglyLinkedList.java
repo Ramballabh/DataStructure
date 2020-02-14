7c4b3811b9758ba6a73b04cd29890079da84efbb
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-02-14 06:40:58 +0530

    SinglyLinkedList implements SinglyADT and implements methods of adt

--- src/definition/SinglyLinkedList.java
+++ src/definition/SinglyLinkedList.java
@@ -1,5 +1,31 @@
 package definition;
 
-public class SinglyLinkedList<E> {
+import adt.SinglyADT;
 
+public class SinglyLinkedList<E> implements SinglyADT<E> {
+
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public void remove(E item) {
+
+    }
+
+    @Override
+    public int search(E item) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
