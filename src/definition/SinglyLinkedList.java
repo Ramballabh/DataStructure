6edc05ea045c9e12f841d674fb942f587c0553ca
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-02-14 06:44:10 +0530

    add constructor and getter and setter method in Node class

--- src/definition/SinglyLinkedList.java
+++ src/definition/SinglyLinkedList.java
@@ -33,6 +33,21 @@ public class SinglyLinkedList<E> implements SinglyADT<E> {
         private E data;
         private Node<E> next;
 
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
 
+        public Node(E data) {
+            this.data = data;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
     }
 }
