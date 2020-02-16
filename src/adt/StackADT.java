3e98e325bf2ca8abfa6035c09b2736ab808da3d8
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-02-16 05:50:35 +0530

    declare the function in an interface

--- src/adt/StackADT.java
+++ src/adt/StackADT.java
@@ -1,5 +1,14 @@
 package adt;
 
 public interface StackADT<E> {
+    public boolean isEmpty();
+
+    public void push(E item);
+
+    public E peek();
+
+    public void pop();
+
+    public void print();
 
 }
