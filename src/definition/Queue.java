2638c3fe1ed9989fd169cfc8a626353510bc6de1
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-02-19 04:55:11 +0530

    create private static Node class

--- src/definition/Queue.java
+++ src/definition/Queue.java
@@ -3,7 +3,9 @@ package definition;
 import adt.QueueADT;
 
 public class Queue<E> implements QueueADT<E> {
-
+    private Node<E> front = null;
+    private Node<E> rear = null;
+    private int size = 0;
 
     @Override
     public void add(E item) {
@@ -34,4 +36,29 @@ public class Queue<E> implements QueueADT<E> {
     public void print() {
 
     }
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+        private Node<E> previous;
+
+        public Node(E data, Node<E> next, Node<E> previous) {
+            this.data = data;
+            this.next = next;
+            this.previous = previous;
+        }
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public Node<E> getPrevious() {
+            return previous;
+        }
+    }
+
 }
