beb304b56f1f680024a7479347279504dde94c3e
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-02-19 04:53:44 +0530

    override all methods from interface

--- src/definition/Queue.java
+++ src/definition/Queue.java
@@ -1,6 +1,37 @@
 package definition;
 
-public class Queue<E> {
+import adt.QueueADT;
+
+public class Queue<E> implements QueueADT<E> {
 
 
+    @Override
+    public void add(E item) {
+
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public E poll() {
+        return null;
+    }
+
+    @Override
+    public E element() {
+        return null;
+    }
+
+    @Override
+    public void print() {
+
+    }
 }
