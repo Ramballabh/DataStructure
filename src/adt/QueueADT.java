d668cbdc15623f095fb9759482ca63043fe108e3
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-02-19 04:51:30 +0530

    declare all the methods ina an interface

--- src/adt/QueueADT.java
+++ src/adt/QueueADT.java
@@ -1,5 +1,21 @@
 package adt;
 
 public interface QueueADT<E> {
+    /**
+     * Inserts item at the rear of the queue.
+     *
+     * @param item the data that is to be stored in the node.
+     * @return true when the node is added to the queue.
+     */
+    void add(E item);
 
+    E remove();
+
+    E peek();
+
+    E poll();
+
+    E element();
+
+    void print();
 }
