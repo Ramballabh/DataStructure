08fb7909813bacd11f5dbf124701434b408415db
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-02-19 05:05:06 +0530

    call all the methods defined in Queue class

--- src/main/MainQ.java
+++ src/main/MainQ.java
@@ -1,5 +1,18 @@
 package main;
 
+import definition.Queue;
+
 public class MainQ {
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<>();
+        queue.add(5);
+        queue.add(6);
+        queue.add(10);
+        System.out.println(queue);
+        queue.peek();
+        queue.remove();
+        System.out.println(queue);
+
+    }
 
 }
